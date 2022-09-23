6767b010103d529400df4a97ad31502657cbcb94
Smilena <damulhan@gmail.com>
2022-09-23 15:25:04 +0900

    Update test_activerecord.rb

--- tests/test_activerecord.rb
+++ tests/test_activerecord.rb
@@ -23,7 +23,7 @@ class CUBRID_ActiveRecordTest < Test::Unit::TestCase
       host: 'localhost',
       username: 'dba',
       password: '',
-      database: 'demodb'
+      database: 'testdb'
     )
 
     @con = adapter.connection
